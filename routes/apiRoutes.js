c619ad688350bcbcedca5825e35e9f533dfddfef
Andrew Dowd <andrewjhdowd@gmail.com>
2024-02-05 23:37:56 -0600

    Adding in text to speech aspect to the document conversion call, but not adding in a dynamic file route yet

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,10 @@ const http = require('https');
 const userRouter = Router();
 const apiRouter = Router();
 const upload = multer();
+const gTTS = require('gtts');
 const rapidAPIKey = process.env.RAPIDAPI_KEY;
 const rapidAPIHost = process.env.RAPIDAPI_HOST;
+const audioPath = process.env.AUDIO_PATH;
 //const mongoClient = require("../db/connection");
 //const ObjectId = require("mongodb").ObjectId;
 //const {User, Entry} = require("../models");
@@ -68,6 +70,24 @@ apiRouter.post('/rapidapi', upload.single('document'), async (request, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   
+  });
+
+  apiRouter.post('/saveAudio', (req, res) => {
+    console.log("Making Audio");
+    //console.log(req.body.text);
+
+    const text = req.body.text;
+    try {
+        var gtts = new gTTS(req.body.text, 'en');
+        gtts.save('public/uploads/test1.mp3', function (err, result) {
+          if(err) { throw new Error(err) }
+          console.log('Success! Open file /tmp/hello.mp3 to hear result.');
+        });
+        res.sendStatus(200);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
   });
 
   module.exports = apiRouter;
